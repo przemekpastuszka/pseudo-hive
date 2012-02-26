1a8b642b4e9093a298ad70c6f39d5c43d755d75f
Przemysław Pastuszka <pastuszka.przemyslaw@gmail.com>
2012-02-26 23:29:11 +0100

    Support for joins added(2)

---
+++ src/tools/RowParser.java
@@ -0,0 +1,27 @@
+package tools;
+
+import database.Row;
+
+public class RowParser {
+  public static void parseValue(String value, Row row) throws MalformedRecordException {
+    boolean readSuccess = row.readFromLine(value);
+    if (readSuccess) {
+      readSuccess = row.isValid();
+    }
+    if (readSuccess == false) {
+      throw new MalformedRecordException();
+    }
+  }
+
+  public static Row instantiateNewRow(Class<? extends Row> clazz) {
+    try {
+      return clazz.newInstance();
+    } catch (InstantiationException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static class MalformedRecordException extends Exception {
+    private static final long serialVersionUID = 931758288441250318L;
+  }
+}
